5e7f95e8089877970c312dcc26fbb98fb155f9ac
Jorge A Gallegos <kad@blegh.net>
2009-11-18 00:15:01 -0800

    I'm tired.

---
+++ src/constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4
+#
+# Bare Naked Blog
+# Copyright 2009, Jorge A Gallegos <kad@blegh.net>
+
+app_name = "BareNaked"
+app_version = "0.1"
+author = "Jorge A Gallegos <kad@blegh.net>"
